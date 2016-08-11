601ba186e75401f46fc79117b8b9af9e90cd3a0d
Joseph <joseph@Josephs-MacBook-Pro.local>
2016-08-10 17:06:09 -0700

    moving onto importing express and lodash.

--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
 console.log("hello typescript 2");
+
+interface Card {
+    name:string;
+    cost:number;
+}
+
+function displayCards(mycards: [Card]) {
+    for (var card in mycards) {
+        console.log(card['name']);
+    }
+    mycards.forEach(function(card: Card) {
+
+        console.log(card.name);
+    });
+}
+
+
+displayCards([{name:"lilliana", cost: 3},{name:"tarmogyof", cost: 2}])
