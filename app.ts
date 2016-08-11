0c4800bc53b74fb0ad3549432cd1eb31a78be9da
Joseph <joseph@Josephs-MacBook-Pro.local>
2016-08-10 18:30:27 -0700

    still not finding express

---
+++ app.ts
@@ -0,0 +1,5 @@
+//import * as express from 'express'    //didn't work, with full node_modules path
+import express = require('express');    //didn't work, "       "
+import * as _ from "lodash";
+console.log(express);
+console.log(_);
