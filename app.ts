ac60802f83415122ee9d906b1c10a414fce9b915
Joseph <jrichardson@jmstudios.net>
2016-09-06 17:55:28 -0700

    first middleware in can make the base endpoint and a way for testing each connection for api-key

--- app.ts
+++ app.ts
@@ -31,7 +31,12 @@ sequelize.authenticate().then(function(err) {
   });
 
 
-
+var firstMiddleware = function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  console.log(req.headers);
+  console.log(Date());
+  console.log("----------------------------");
+  next();
+};
 
 var end = new EndpointsRouter();
 app.use('/endpoints', end.router);
@@ -46,6 +51,7 @@ router.route('/api')
                     }).post(function(req, res) {
 
                     });
+app.use(firstMiddleware);
 app.use('/', router);
 app.listen(8000, function(){
     console.log("Demo Express server listening on port %d", 8000);
